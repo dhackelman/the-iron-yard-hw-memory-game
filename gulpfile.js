d55d872469f6e04e44f7169826da38c559408ddf
dhackelman <drew.hackelman@gmail.com>
2017-02-13 15:46:20 -0500

    include gulp-ed workflow

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+"use strict"
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
+
+
+gulp.task('sass', function() {
+  return gulp.src('./src/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./lib/styles/'));
+});
+
+gulp.task('js', function() {
+  gulp.src('./src/js/vendor/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./lib/js/vendor'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('./src/styles/**/*.scss', ['sass']);
+  gulp.watch('./src/js/*.js', ['js']);
+});
+
+gulp.task('default', ['watch', 'sass', 'js']);
